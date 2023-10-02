59a5f72e6294d65e7de7353e3409dcf4dbeb534c
kat0d1 <60965269+Kolian112@users.noreply.github.com>
2023-10-02 21:19:45 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 var admin = require("firebase-admin");
 
-var serviceAccount = require("text-to-speech-node-firebase-adminsdk-q1jir-005d5e50c1.json");
+var serviceAccount = {
+"type": "service_account",
+"project_id": "text-to-speech-node",
+"private_key_id": process.env.PRIVATE_KEY_ID,
+"private_key": process.env.PRIVATE_KEY,
+"client_email": "firebase-adminsdk-q1jir@text-to-speech-node.iam.gserviceaccount.com",
+"client_id": process.env.CLIENT_ID,
+"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+"token_uri": "https://oauth2.googleapis.com/token",
+"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+"client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-q1jir%40text-to-speech-node.iam.gserviceaccount.com",
+"universe_domain": "googleapis.com"
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -11,7 +24,6 @@ const db = admin.firestore();
 var TelegramBot = require('node-telegram-bot-api');
 var gTTs = require('gtts');
 
-require('dotenv').config();
 const token = process.env.TOKEN;
 
 var bot = new TelegramBot(token, { polling: true});
