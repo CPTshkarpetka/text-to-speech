b4e6f3b79240b82ed1f960538e4c783926c2435a
kat0d1 <60965269+Kolian112@users.noreply.github.com>
2023-10-03 23:10:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var gTTs = require('gtts');
 
 const token = process.env.TOKEN;
 
-var bot = new TelegramBot(token, { polling: true});
+var bot = new TelegramBot(token);
 bot.setWebHook(process.env.URL);
 var userID;
 const fs = require('fs');
