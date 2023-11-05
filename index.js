1f68c4b80f078abfed08ae0b05863aeb0c94bf25
kat0d1 <60965269+CPTshkarpetka@users.noreply.github.com>
2023-11-05 22:29:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const token = process.env.TOKEN;
 
 var bot = new TelegramBot(token, { webHook:{
     host:"0.0.0.0",
-    port: process.env.PORT || 5000
+    port: 8080 || process.env.PORT
 }});
 
 bot.setWebHook(process.env.URL);
@@ -36,7 +36,7 @@ bot.setMyCommands([
 bot.onText(/\/start/, (msg) => {
     userID = msg.chat.id;
     bot.sendMessage(userID,
-        `*Welcome to this bot\\. Here you can make your text a voice message witch /tospeech \\*your text\\*
+        `*Welcome to this bot\\. Here you can make your text a voice message with /tospeech \\*your text\\*
 Now 2 languages are available\: english and ukrainian\\. To switch between them use /language*`,
         { parse_mode: "MarkdownV2" });
     db.collection("users").doc(msg.from.username).set({ lang: "uk-UA" });
@@ -44,17 +44,35 @@ Now 2 languages are available\: english and ukrainian\\. To switch between them 
 
 bot.onText(/\/language/, async (msg) => {
     userID = msg.chat.id;
-    var deleteMessage = await bot.sendMessage(userID, "What language do you want to choose?", {
-        "reply_markup": {
-            "inline_keyboard": [[{ text: "english", callback_data: "english" }, { text: "ukrainian", callback_data: "ukrainian" }]]
-        }
-    })
+    db.collection("users").doc(msg.from.username).get().then(doc => { language = doc.data().lang });
     setTimeout(async () => {
-        try {
-            await bot.deleteMessage(userID, deleteMessage.message_id);
+        if (language == "uk-UA") {
+            var deleteMessage = await bot.sendMessage(userID, "Яку мову ви хочете обрати?", {
+                "reply_markup": {
+                    "inline_keyboard": [[{ text: "англійська", callback_data: "english" }, { text: "українська", callback_data: "ukrainian" }]]
+                }
+            })
+            setTimeout(async () => {
+                try {
+                    await bot.deleteMessage(userID, deleteMessage.message_id);
+                }
+                catch (err) { }
+            }, 50000)  
+        } else if (language == "en-US") {
+            var deleteMessage = await bot.sendMessage(userID, "What language do you want to choose?", {
+                "reply_markup": {
+                    "inline_keyboard": [[{ text: "english", callback_data: "english" }, { text: "ukrainian", callback_data: "ukrainian" }]]
+                }
+            })
+            setTimeout(async () => {
+                try {
+                    await bot.deleteMessage(userID, deleteMessage.message_id);
+                }
+                catch (err) { }
+            }, 50000)
         }
-        catch (err) { }
-    }, 50000)
+    }, 550)
+     
 });
 
 bot.on('callback_query', (msg) => {
@@ -63,10 +81,18 @@ bot.on('callback_query', (msg) => {
         db.collection("users").doc(msg.from.username).update({ lang: "en-US" });
         bot.sendMessage(userID, "Language was set to english");
         bot.deleteMessage(userID, msg.message.message_id);
+        bot.setMyCommands([
+            { command: '/tospeech', description: "Converting text to speech" },
+            { command: '/language', description: "Changing language of speech" }
+        ])
     } else if (msg.data == "ukrainian") {
         db.collection("users").doc(msg.from.username).update({ lang: "uk-UA" });
         bot.sendMessage(userID, "Мова була змінена на українську");
         bot.deleteMessage(userID, msg.message.message_id);
+        bot.setMyCommands([
+            { command: '/tospeech', description: "Конвертує текст у аудіо" },
+            { command: '/language', description: "Змінити мову" }
+        ])
     }
 })
 
